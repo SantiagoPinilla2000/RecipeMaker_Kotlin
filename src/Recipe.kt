cb4a98050ed1d6d8cc91b8beac85e47ee320e4b3
Santiago Pinilla <johansantiagopinillauribe@gmail.com>
2020-02-16 15:47:42 -0500

    funcion makeRecipe completada

--- src/Recipe.kt
+++ src/Recipe.kt
@@ -1,73 +1,185 @@
 import com.sun.org.apache.bcel.internal.generic.SWITCH
 
-fun main(args : Array<String>){
+fun main(args: Array<String>) {
     //RecipeMaker_2020
     println("----------Bienvenido a Recipe Maker---------- \n¿Que te apetece preparar hoy?")
     begin()
 }
-fun begin(){
-    println("Seleccione una de las siguientes opciones:" +
-            "\n1. Hacer una receta.\n2. Ver mis recetas.\n3.Salir")
-    val respuestaUsuario : String? = readLine()
 
-    when(respuestaUsuario){
-        "1" -> receta()
-        "2" -> verReceta()
-        "3" -> println("\nGracias, Vuelve pronto y nunca pares de cocinar ;)")
-        else -> println("Selecciona una opcion valida."+ begin())
+fun begin() {
+    println(
+        "Seleccione una de las siguientes opciones:" +
+                "\n1. Hacer una receta.\n2. Ver mis recetas.\n3.Salir"
+    )
+    val respuestaUsuario: String? = readLine()
+
+    when (respuestaUsuario) {
+        "1", "hacer una receta" -> makeRecipe()
+        "2", "ver mis recetas" -> viewRecipe()
+        "3", "salir" -> println("\nGracias, Vuelve pronto y nunca pares de cocinar ;)")
+        else -> println("Selecciona una opcion valida." + begin())
     }
 }
-fun receta(){
-    val ingredientes: List<String> = listOf("1. Agua\n" +
-            "2. Leche\n" +
-            "3. Carne\n" +
-            "4. Verduras\n" +
-            "5. Frutas\n" +
-            "6. Cereal\n" +
-            "7. Huevos\n" +
-            "8. Aceite")
-    println("Selecciona los ingredientes: $ingredientes")
+
+fun makeRecipe() {
+    val ingredientes: Array<String> =
+        arrayOf("Agua.", "Leche.", "Carne.", "Verduras.", "Frutas.", "Cereal.", "Huevos.", "Aceite.")
+    println("Selecciona los ingredientes:\n")
+    for (i in ingredientes) {
+        println("$i")
+    }
+    println("\n¿Cual de los ingredientes quieres utilizar?:\n")
     val respuestaUsuario2: String? = readLine()
-    if (respuestaUsuario2 == "1"){
-        val agua = "Agua"
-        println(agua)
+    if (respuestaUsuario2.equals("1") || respuestaUsuario2.equals("agua")) {
+        println("El ingrediente que seleccionaste es: ${ingredientes[0]}")
+        println(
+            "\n¿Deseas continuar y ver tus recetas o quieres salir del sistema?" +
+                    "\n1. Continuar a recetas" +
+                    "\n2. Salir"
+        )
+        val respuestaUsuario3: String? = readLine()
+        if (respuestaUsuario3.equals("1") || respuestaUsuario3.equals("continuar a recetas")) {
+            viewRecipe()
+        }
+        if (respuestaUsuario3.equals("2") || respuestaUsuario3.equals("salir")) {
+            println("\nGracias, Vuelve pronto y nunca pares de cocinar ;)")
+        } else {
+            println("Ingresa una opcion valida.\n")
+            makeRecipe()
+        }
     }
-    if (respuestaUsuario2 == "2"){
-        val leche = "Leche"
-        println(leche)
+    if (respuestaUsuario2.equals("2") || respuestaUsuario2.equals("leche")) {
+        println("El ingrediente que seleccionaste es: ${ingredientes[1]}")
+        println(
+            "\n¿Deseas continuar y ver tus recetas o quieres salir del sistema?" +
+                    "\n1. Continuar a recetas" +
+                    "\n2. Salir"
+        )
+        val respuestaUsuario3: String? = readLine()
+        if (respuestaUsuario3.equals("1") || respuestaUsuario3.equals("continuar a recetas")) {
+            viewRecipe()
+        }
+        if (respuestaUsuario3.equals("2") || respuestaUsuario3.equals("salir")) {
+            println("\nGracias, Vuelve pronto y nunca pares de cocinar ;)")
+        } else {
+            println("Ingresa una opcion valida.\n")
+            makeRecipe()
+        }
     }
-    if (respuestaUsuario2 == "3"){
-        val carne = "Carne"
-        println(carne)
+    if (respuestaUsuario2.equals("3") || respuestaUsuario2.equals("carne")) {
+        println("El ingrediente que seleccionaste es: ${ingredientes[2]}")
+        println(
+            "\n¿Deseas continuar y ver tus recetas o quieres salir del sistema?" +
+                    "\n1. Continuar a recetas" +
+                    "\n2. Salir"
+        )
+        val respuestaUsuario3: String? = readLine()
+        if (respuestaUsuario3.equals("1") || respuestaUsuario3.equals("continuar a recetas")) {
+            viewRecipe()
+        }
+        if (respuestaUsuario3.equals("2") || respuestaUsuario3.equals("salir")) {
+            println("\nGracias, Vuelve pronto y nunca pares de cocinar ;)")
+        } else {
+            println("Ingresa una opcion valida.\n")
+            makeRecipe()
+        }
     }
-    if (respuestaUsuario2 == "4"){
-        val verduras = "Verduras"
-        println(verduras)
+    if (respuestaUsuario2.equals("4") || respuestaUsuario2.equals("verduras")) {
+        println("El ingrediente que seleccionaste es: ${ingredientes[3]}")
+        println(
+            "\n¿Deseas continuar y ver tus recetas o quieres salir del sistema?" +
+                    "\n1. Continuar a recetas" +
+                    "\n2. Salir"
+        )
+        val respuestaUsuario3: String? = readLine()
+        if (respuestaUsuario3.equals("1") || respuestaUsuario3.equals("continuar a recetas")) {
+            viewRecipe()
+        }
+        if (respuestaUsuario3.equals("2") || respuestaUsuario3.equals("salir")) {
+            println("\nGracias, Vuelve pronto y nunca pares de cocinar ;)")
+        } else {
+            println("Ingresa una opcion valida.\n")
+            makeRecipe()
+        }
     }
-    if (respuestaUsuario2 == "5"){
-        val frutas = "Frutas"
-        println(frutas)
+    if (respuestaUsuario2.equals("5") || respuestaUsuario2.equals("frutas")) {
+        println("El ingrediente que seleccionaste es: ${ingredientes[4]}")
+        println(
+            "\n¿Deseas continuar y ver tus recetas o quieres salir del sistema?" +
+                    "\n1. Continuar a recetas" +
+                    "\n2. Salir"
+        )
+        val respuestaUsuario3: String? = readLine()
+        if (respuestaUsuario3.equals("1") || respuestaUsuario3.equals("continuar a recetas")) {
+            viewRecipe()
+        }
+        if (respuestaUsuario3.equals("2") || respuestaUsuario3.equals("salir")) {
+            println("\nGracias, Vuelve pronto y nunca pares de cocinar ;)")
+        } else {
+            println("Ingresa una opcion valida.\n")
+            makeRecipe()
+        }
     }
-    if (respuestaUsuario2 == "6"){
-        val cereal = "Cereal"
-        println(cereal)
+    if (respuestaUsuario2.equals("6") || respuestaUsuario2.equals("cereal")) {
+        println("El ingrediente que seleccionaste es: ${ingredientes[5]}")
+        println(
+            "\n¿Deseas continuar y ver tus recetas o quieres salir del sistema?" +
+                    "\n1. Continuar a recetas" +
+                    "\n2. Salir"
+        )
+        val respuestaUsuario3: String? = readLine()
+        if (respuestaUsuario3.equals("1") || respuestaUsuario3.equals("continuar a recetas")) {
+            viewRecipe()
+        }
+        if (respuestaUsuario3.equals("2") || respuestaUsuario3.equals("salir")) {
+            println("\nGracias, Vuelve pronto y nunca pares de cocinar ;)")
+        } else {
+            println("Ingresa una opcion valida.\n")
+            makeRecipe()
+        }
     }
-    if (respuestaUsuario2 == "7"){
-        val huevos = "Huevos"
-        println(huevos)
+    if (respuestaUsuario2.equals("7") || respuestaUsuario2.equals("huevos")) {
+        println("El ingrediente que seleccionaste es: ${ingredientes[6]}")
+        println(
+            "\n¿Deseas continuar y ver tus recetas o quieres salir del sistema?" +
+                    "\n1. Continuar a recetas" +
+                    "\n2. Salir"
+        )
+        val respuestaUsuario3: String? = readLine()
+        if (respuestaUsuario3.equals("1") || respuestaUsuario3.equals("continuar a recetas")) {
+            viewRecipe()
+        }
+        if (respuestaUsuario3.equals("2") || respuestaUsuario3.equals("salir")) {
+            println("\nGracias, Vuelve pronto y nunca pares de cocinar ;)")
+        } else {
+            println("Ingresa una opcion valida.\n")
+            makeRecipe()
+        }
     }
-    if (respuestaUsuario2 == "8"){
-        val aceite = "Aceite"
-        println(aceite)
+    if (respuestaUsuario2.equals("8") || respuestaUsuario2.equals("aceite")) {
+        println("El ingrediente que seleccionaste es: ${ingredientes[7]}")
+        println(
+            "\n¿Deseas continuar y ver tus recetas o quieres salir del sistema?" +
+                    "\n1. Continuar a recetas" +
+                    "\n2. Salir"
+        )
+        val respuestaUsuario3: String? = readLine()
+        if (respuestaUsuario3.equals("1") || respuestaUsuario3.equals("continuar a recetas")) {
+            viewRecipe()
+        }
+        if (respuestaUsuario3.equals("2") || respuestaUsuario3.equals("salir")) {
+            println("\nGracias, Vuelve pronto y nunca pares de cocinar ;)")
+        } else {
+            println("Ingresa una opcion valida.\n")
+            makeRecipe()
+        }
+    } else {
+        println("Por favor selecciona una opcion valida.")
+        makeRecipe()
     }
-    else{
-        println("\nPor favor selecciona una opcion valida. Presiona ENTER para continuar")
-        val enter: String? = readLine()
-        receta()
-    }
-
 }
-fun verReceta(){
+
+fun viewRecipe() {
     println("Aqui estan tus recetas:\nPor el momento se encuentra vacia la lista de tus recetas.")
     begin()
 }
